0101ae9feaaba8d24eb7e90582c658b4fac14a0d
Jeremy Sorensen <Jeremy.Sorensen@analog.com>
2018-09-10 16:27:18 -0700

    moved stuff around

---
+++ Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Demo.AspNetCore.WebSockets.Infrastructure;
+using Demo.AspNetCore.WebSockets.Services;
+using Demo.AspNetCore.WebSockets.Middlewares;
+
+namespace Demo.AspNetCore.WebSockets
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebSocketCompression();
+            services.AddWebSocketConnections();
+
+            services.AddSingleton<IHostedService, HeartbeatService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("websocket-api.html");
+
+            ITextWebSocketSubprotocol textWebSocketSubprotocol = new PlainTextWebSocketSubprotocol();
+            WebSocketConnectionsOptions webSocketConnectionsOptions = new WebSocketConnectionsOptions
+            {
+                AllowedOrigins = new HashSet<string> { "http://localhost:5000" },
+                SupportedSubProtocols = new List<ITextWebSocketSubprotocol>
+                {
+                    new JsonWebSocketSubprotocol(),
+                    textWebSocketSubprotocol
+                },
+                DefaultSubProtocol = textWebSocketSubprotocol,
+                SendSegmentSize = 4 * 1024
+            };
+
+            app.UseDefaultFiles(defaultFilesOptions)
+                .UseStaticFiles()
+                .UseWebSocketsCompression()
+                .MapWebSocketConnections("/socket", webSocketConnectionsOptions)
+                .Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("-- Demo.AspNetCore.WebSocket --");
+                });
+        }
+    }
+}
